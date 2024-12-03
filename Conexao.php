b5b527f82d56e9dde1e5e4d4759d418475f75932
Marcus <mvvideos10@gmail.com>
2024-12-02 22:17:32 -0300

    Initial commit: Create Conexao.php for database connection

---
+++ Conexao.php
@@ -0,0 +1,24 @@
+<?php
+// script.php
+
+// Configurações de conexão com o banco de dados
+$host = 'localhost';
+$db = 'AppAgendaWe';
+$user = 'marcus';
+$pass = '20251426';
+$charset = 'utf8mb4';
+
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+];
+
+try {
+    $conexao = new PDO($dsn, $user, $pass, $options);
+    echo "Conexão bem-sucedida!";
+} catch (\PDOException $e) {
+    throw new \PDOException($e->getMessage(), (int)$e->getCode());
+}
+?>
